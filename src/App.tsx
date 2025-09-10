616c4fe8be08230cfca214b1b27ed1db2a0206b6
Brian Duong <dbiny3css@gmail.com>
2025-09-09 20:34:47 -0500

    Initial commit
    I've basically cloned my repo below which contained all git history up
    to commit cf4ddfc:
    "https://github.com/BDuong0/vite-react-ts-stylex-tailwind-starter"

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <main>
+      <h1>SOS Game</h1>
+      <p>I will flesh out the UI later</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam commodi
+        voluptates nihil labore perspiciatis veritatis quia dolorem porro
+        delectus, unde doloribus debitis similique aliquid error vel. Mollitia
+        cum obcaecati doloribus.
+      </p>
+
+      <label>
+        <input type="checkbox" id="checkbox1" name="checkbox1" />
+        <span>Checkbox 1</span>
+      </label>
+
+      <label>
+        <input type="radio" name="radiobutton1" />
+        <span>Radio Button 1</span>
+      </label>
+
+      <label>
+        <input type="radio" name="radiobutton2" />
+        <span>Radio Button 2</span>
+      </label>
+    </main>
+  );
+}
+
+export default App;
