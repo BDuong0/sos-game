e68e0f970ac598a91d32515d3f475b96b8546405
Brian Duong <dbiny3css@gmail.com>
2025-10-20 10:41:59 -0500

    feat(feature): Create Board class

---
+++ src/features/board.ts
@@ -0,0 +1,27 @@
+export class Board {
+  public grid: number[][];
+  public size: number[];
+
+  constructor(
+    private rows: number = 3,
+    private columns: number = 3,
+  ) {
+    this.rows = this.isBoardSizeValid(rows, columns) ? rows : 3;
+    this.columns = this.isBoardSizeValid(rows, columns) ? columns : 3;
+    this.grid = Array.from({ length: this.rows }, () => Array(this.columns).fill(0));
+    this.size = [this.rows, this.columns];
+  }
+
+  private isBoardSizeValid(rows: number, columns: number): boolean {
+    if (rows != columns) {
+        return false
+    }
+    
+    if (3 <= rows && rows <= 8) {
+        return true
+    } else {
+        return false
+    }
+    
+  }
+}
