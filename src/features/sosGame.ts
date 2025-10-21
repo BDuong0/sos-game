8606d9d75938747b001b69e71cf6d7f94cc6aca0
Brian Duong <dbiny3css@gmail.com>
2025-10-21 12:10:44 -0500

    feat(features): Create SoSGame and Player class
    Player class is used as a private property inside the SoSGame class

--- src/features/sosGame.ts
+++ src/features/sosGame.ts
@@ -9,11 +9,15 @@ export class SoSGame {
     public board: Board
     private gameMode: gameModes 
     private whoseTurn: number
+    private bluePlayer: Player
+    private redPlayer: Player
 
     constructor(gameMode: gameModes = gameModes.Simple) {
         this.board = new Board()
         this.gameMode = gameMode
         this.whoseTurn = 1
+        this.bluePlayer = new Player("S")
+        this.redPlayer = new Player("O")
     }
 
     public getGameMode(){
@@ -30,5 +34,19 @@ export class SoSGame {
 
     public setWhoseTurn(player: number) {
         this.whoseTurn = player
+    }
+}
+
+type playerSymbols = "S" | "O"
+
+class Player {
+    private playerSymbol: playerSymbols
+
+    constructor(playerSymbol: playerSymbols){
+        this.playerSymbol = playerSymbol
+    }
+
+    public getPlayerSymbol() {
+        return this.playerSymbol
     }
 }
