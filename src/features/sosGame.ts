e83130779e4e97d76a1b57b18feb56d55268d50e
Brian Duong <dbiny3css@gmail.com>
2025-10-21 08:31:38 -0500

    feat(features): Create SoSGame Class

---
+++ src/features/sosGame.ts
@@ -0,0 +1,24 @@
+import { Board } from "./board";
+
+export enum gameModes {
+  Simple = "SIMPLE",
+  General = "GENERAL",
+}
+
+export class SoSGame {
+    private board: Board
+    private gameMode: gameModes 
+
+    constructor(gameMode: gameModes = gameModes.Simple) {
+        this.board = new Board()
+        this.gameMode = gameMode
+    }
+
+    public getGameMode(){
+        return this.gameMode
+    }
+
+    public setGameMode(gameMode: gameModes) {
+        this.gameMode = gameMode
+    }
+}
