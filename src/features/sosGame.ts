3577c9329e909795baaff04d162d3a1d49548069
Brian Duong <dbiny3css@gmail.com>
2025-11-02 20:23:12 -0600

    refactor(sosGame.ts): Delete Player class definition
    I moved the Player class definition into a separate file (player.ts).
    I also deleted the bluePlayer and redPlayer private properties of SoSGame,
    because I might want to use dependency injection to inject instances of
    Player into an instance of SoSGame.

--- src/features/sosGame.ts
+++ src/features/sosGame.ts
@@ -14,15 +14,11 @@ export class SoSGame {
     public board: Board
     private gameMode: gameModes 
     private whoseTurn: gamePlayers
-    private bluePlayer: Player
-    private redPlayer: Player
 
     constructor(gameMode: gameModes = gameModes.Simple) {
         this.board = new Board()
         this.gameMode = gameMode
         this.whoseTurn = gamePlayers.Blue
-        this.bluePlayer = new Player("S")
-        this.redPlayer = new Player("O")
     }
 
     public getGameMode(){
@@ -39,19 +35,5 @@ export class SoSGame {
 
     public setWhoseTurn(player: gamePlayers) {
         this.whoseTurn = player
-    }
-}
-
-type playerSymbols = "S" | "O"
-
-class Player {
-    private playerSymbol: playerSymbols
-
-    constructor(playerSymbol: playerSymbols){
-        this.playerSymbol = playerSymbol
-    }
-
-    public getPlayerSymbol() {
-        return this.playerSymbol
     }
 }
