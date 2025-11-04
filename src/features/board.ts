e7206f875460487078ff996c164b317564867e81
Brian Duong <dbiny3css@gmail.com>
2025-11-03 19:56:43 -0600

    feat(board.ts): Make Board class a type generic class
    I wanted to allow the user to define what type of values the board accepts
    whether it be string or number.

--- src/features/board.ts
+++ src/features/board.ts
@@ -1,14 +1,17 @@
-export class Board {
-  public grid: number[][];
+export type CellValuesType<V> = { empty: V } & Record<string, V>;
+
+export class Board<T> {
+  public grid: T[][];
   public size: number[];
 
   constructor(
+    public readonly cellValues: CellValuesType<T>,
     private rows: number = 3,
     private columns: number = 3,
   ) {
     this.rows = this.isBoardSizeValid(rows, columns) ? rows : 3;
     this.columns = this.isBoardSizeValid(rows, columns) ? columns : 3;
-    this.grid = Array.from({ length: this.rows }, () => Array(this.columns).fill(0));
+    this.grid = Array.from({ length: this.rows }, () => Array(this.columns).fill(this.cellValues.empty));
     this.size = [this.rows, this.columns];
   }
 
@@ -35,7 +38,7 @@ export class Board {
     return this.grid[rowIndex][columnIndex]
   }
 
-  public editCellValue(rowIndex: number, columnIndex: number, value: number) {
+  public editCellValue(rowIndex: number, columnIndex: number, value: T) {
     this.grid[rowIndex][columnIndex] = value
   }
 }
