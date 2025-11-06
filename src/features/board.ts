0e5ef1638b1e97d114085d5bf47200925331293b
Brian Duong <dbiny3css@gmail.com>
2025-11-06 08:03:14 -0600

    feat(board.ts): Change data type for grid public property
    I realized that if I wanted to be able to determine who the winner is an
    for the SoSGame, I couldn't have my Board class just accept one type of
    data type, so I wanted to allow Board class to accept an object type or
    an array. When I play the SoSGame, no only do I need to track what symbols
    ('S' or 'O') are in each cell, but I also need to track which Player placed
    the symbols in each cell, because I misunderstood that SoS players had to
    stick to 'S' or 'O' the entire game, but they can actually choose to place
    ether an 'S' or an 'O' during their turn.

--- src/features/board.ts
+++ src/features/board.ts
@@ -1,18 +1,33 @@
 export type CellValuesType<V> = { empty: V } & Record<string, V>;
 
 export class Board<T> {
-  public grid: T[][];
+  public grid: (keyof CellValuesType<T> | [keyof CellValuesType<T>, any])[][];
   public size: number[];
 
   constructor(
     public readonly cellValues: CellValuesType<T>,
     private rows: number = 3,
     private columns: number = 3,
+    private setCellValuesAsArray: boolean = false
   ) {
     this.rows = this.isBoardSizeValid(rows, columns) ? rows : 3;
     this.columns = this.isBoardSizeValid(rows, columns) ? columns : 3;
-    this.grid = Array.from({ length: this.rows }, () => Array(this.columns).fill(this.cellValues.empty));
+    this.setCellValuesAsArray = setCellValuesAsArray
+
+    const initialCellValue = this.createInitialCellValue();
+    this.grid = Array.from({ length: this.rows }, () => Array(this.columns).fill(initialCellValue));
     this.size = [this.rows, this.columns];
+  }
+
+  private createInitialCellValue(): keyof CellValuesType<T> | [keyof CellValuesType<T>, any] {
+    const initialCellValue = this.cellValues.empty as keyof CellValuesType<T>
+
+    if (this.setCellValuesAsArray) {
+      return [initialCellValue, null]
+    }
+    else {
+      return initialCellValue
+    }
   }
 
   private isBoardSizeValid(rows: number, columns: number): boolean {
@@ -34,11 +49,11 @@ export class Board<T> {
     this.size = [this.rows, this.columns];
   }
 
-  public getCellValue(rowIndex: number, columnIndex: number) {
+  public getCellValue(rowIndex: number, columnIndex: number): keyof CellValuesType<T> | [keyof CellValuesType<T>, any] {
     return this.grid[rowIndex][columnIndex]
   }
 
-  public editCellValue(rowIndex: number, columnIndex: number, value: T) {
+  public editCellValue(rowIndex: number, columnIndex: number, value: keyof CellValuesType<T> | [keyof CellValuesType<T>, any]) {
     this.grid[rowIndex][columnIndex] = value
   }
 }
