c9a9c06276b7dcb2c39bee8c0267ae400af1353c
Brian Duong <dbiny3css@gmail.com>
2025-12-05 17:18:44 -0600

    feat(features): Created RecordedSoSGame class
    I've moved all of my game recording code that I've initially created in
    record-simple-game.cy.js into this class so I can later export this class,
    so that my SoS Game UI can use it in App.tsx.

---
+++ src/features/recordedSoSGame.ts
@@ -0,0 +1,56 @@
+import { ComputerPlayer } from "./player";
+import { SimpleSoSGame, SoSGame } from "./sosGame";
+
+export class RecordedSoSGame {
+    private sosGame: SoSGame
+    private recordedMoves: string
+    private recordedGameSettings: string
+    private sosTurnRecorder
+    private textFileContent: string
+    
+    constructor(sosGame: SoSGame) {
+        this.sosGame = sosGame
+        this.recordedMoves = ``
+        this.recordedGameSettings = ``
+        this.sosTurnRecorder = sosGame.latestMoveSubject.subscribe((latestMove) => {
+            // Everytime the SoSGame.latestValidMove changes, this function will process the new value
+            // of SoSGame.latestValidMove, so every completed SoS Game turn gets recorded into one long string
+            if (latestMove.length == 0) return
+        
+            const [turnCount, currentPlayerTurn, rowIndex, columnIndex] = latestMove
+            const recordedTurn = `${turnCount}. ${currentPlayerTurn.getPlayerName() == "Blue Player" ? "P1" : "P2"}:${currentPlayerTurn.getPlayerSymbol()}:${rowIndex}-${columnIndex} `
+            this.recordedMoves += recordedTurn
+        })
+        this.textFileContent = ``
+    }
+
+    public getRecordedMoves() {
+        return this.recordedMoves
+    }
+
+    public getRecordedGameSettings() {
+        const boardSize = `${this.sosGame.board.size[0]}x${this.sosGame.board.size[1]}`
+        const bluePlayerName = this.sosGame.getPlayers()[0].getPlayerName()
+        const bluePlayerType = this.sosGame.getPlayers()[0] instanceof ComputerPlayer ? "COMPUTER" : "HUMAN"
+        const redPlayerName = this.sosGame.getPlayers()[1].getPlayerName()
+        const redPlayerType = this.sosGame.getPlayers()[1] instanceof ComputerPlayer ? "COMPUTER" : "HUMAN"
+        const winner = this.sosGame.determineWinner()
+
+        this.recordedGameSettings = 
+`[gameMode: ${(this.sosGame instanceof SimpleSoSGame) ? "SIMPLE" : "GENERAL"}]
+[boardSize: ${boardSize}]
+[P1: ${bluePlayerName}]
+[P1 Type: ${bluePlayerType}]
+[P2: ${redPlayerName}
+[P2 Type: ${redPlayerType}]
+[Winner: ${winner == undefined ? "None" : winner.getPlayerName()}]`
+        
+        return this.recordedGameSettings
+    }
+
+    public getTextFileContent() {
+        // I don't know how to handle not wanting this method called more than 1 time, because it would make
+        // sense fo have 2 copies of the recorded game settings and recorded moves the single text file
+        return this.textFileContent += this.recordedGameSettings + `\n\n${this.recordedMoves}`
+    }
+}
