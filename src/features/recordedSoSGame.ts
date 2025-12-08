ccd955324b53c9fee1bbfa0f9ee6eefa5d028567
Brian Duong <dbiny3css@gmail.com>
2025-12-08 16:11:29 -0600

    feat(recordedSoSGame.ts): Add methods for creating & exporting the recorded SoS game

--- src/features/recordedSoSGame.ts
+++ src/features/recordedSoSGame.ts
@@ -1,3 +1,4 @@
+import { saveAs } from 'file-saver';
 import { ComputerPlayer } from "./player";
 import { SimpleSoSGame, SoSGame } from "./sosGame";
 
@@ -7,6 +8,7 @@ export class RecordedSoSGame {
     private recordedGameSettings: string
     private sosTurnRecorder
     private textFileContent: string
+    private fileName: string
     
     constructor(sosGame: SoSGame) {
         this.sosGame = sosGame
@@ -22,6 +24,29 @@ export class RecordedSoSGame {
             this.recordedMoves += recordedTurn
         })
         this.textFileContent = ``
+        
+        function generateFileName() {
+            let gameType = ""
+            let playerType = ""    
+            
+            if (sosGame instanceof SimpleSoSGame) {
+                gameType = "simple-game"
+            } else {
+                gameType = "general-game"
+            }
+
+            for (let i = 0; i < sosGame.getPlayers().length; i++){
+                if (sosGame.getPlayers()[i] instanceof ComputerPlayer) {
+                    playerType += "C"
+                } else {
+                    playerType += "H"
+                }
+            }
+
+            return `${gameType}-${playerType}.txt`
+        }
+
+        this.fileName = generateFileName()
     }
 
     public getRecordedMoves() {
@@ -41,7 +66,7 @@ export class RecordedSoSGame {
 [boardSize: ${boardSize}]
 [P1: ${bluePlayerName}]
 [P1 Type: ${bluePlayerType}]
-[P2: ${redPlayerName}
+[P2: ${redPlayerName}]
 [P2 Type: ${redPlayerType}]
 [Winner: ${winner == undefined ? "None" : winner.getPlayerName()}]`
         
@@ -51,6 +76,15 @@ export class RecordedSoSGame {
     public getTextFileContent() {
         // I don't know how to handle not wanting this method called more than 1 time, because it would make
         // sense fo have 2 copies of the recorded game settings and recorded moves the single text file
-        return this.textFileContent += this.recordedGameSettings + `\n\n${this.recordedMoves}`
+        return this.textFileContent += this.getRecordedGameSettings() + `\n\n${this.recordedMoves}`
+    }
+
+    public getFileName() {
+        return this.fileName
+    }
+
+    public downloadTextFile() {
+        const file = new Blob([this.getTextFileContent()], { type: 'text/plain;charset=utf-8' });
+        saveAs(file, this.getFileName());        
     }
 }
